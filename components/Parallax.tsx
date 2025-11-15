272e6bf8b1e234cbdf7041669a45b9f8b4345123
Elijah <106408336+eliegamala@users.noreply.github.com>
2025-11-15 21:29:47 +0200

    Update Parallax.tsx

--- components/Parallax.tsx
+++ components/Parallax.tsx
@@ -2,12 +2,8 @@ export default function Parallax({ image, children }: { image: string, children?
   return (
     <section className="section relative overflow-hidden">
       <div 
-        className="w-full bg-cover bg-center bg-no-repeat md:bg-fixed"
-        style={{ 
-          backgroundImage: `url(${image})`,
-          height: '360px',
-          backgroundAttachment: window.innerWidth >= 768 ? 'fixed' : 'scroll'
-        }}
+        className="parallax w-full h-[360px] sm:h-[400px] md:h-[500px] bg-fixed bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${image})` }}
       />
       {children && (
         <div className="container-narrow mt-8 px-4 mx-auto">
